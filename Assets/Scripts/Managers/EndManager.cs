9f901541ce6960422f01b2152635182b7eed65e3
Deandre Metoyer <deepwns@live.com>
2017-01-28 20:17:09 -0600

    UI and Bugfixes`
    Completely laid groundwork for UI
    Fixed a bug with Time.Scale and Pausing
    Ui Added:
    Pause Menu, Game over, ScoreBoard, Main Menu
    Main menu Cursor, Time, Health and Energy, Score

--- Assets/Scripts/Managers/EndManager.cs
+++ Assets/Scripts/Managers/EndManager.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class EndManager : MonoBehaviour {
+public class EndManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    [SerializeField]
+    private Text endScore;
+
+    [SerializeField]
+    private Text survived;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        endScore.text = ""+ GameManager.score;
+        string minutes = Mathf.Floor(GameManager.endTime / 60).ToString("00");
+        string seconds = (GameManager.endTime % 60).ToString("00");
+        survived.text = minutes + ":" + seconds;
         if (Input.GetKeyDown(KeyCode.Return))
         {
             SceneManager.LoadScene("MainMenu");
         }
-	}
+    }
 }
